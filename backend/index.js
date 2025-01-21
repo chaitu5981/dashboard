83c4a9f8c7f3027aa96f13a5f8ba68332507cc38
Chaitanya <chaitu.raju@gmail.com>
2025-01-21 23:56:46 +0530

    final touch3

--- backend/index.js
+++ backend/index.js
@@ -6,30 +6,41 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.get("/partners", (req, res) => {
-  res.json({ data });
+  fs.readFile("./db.json", "utf8", (err, data) => {
+    if (err) console.log(err);
+    else res.json({ data: JSON.parse(data) });
+  });
 });
 app.post("/partners/create", (req, res) => {
   const data1 = data;
-  let id;
-  if (data1.length === 0) id = 1;
-  else id = data1[data1.length - 1].id + 1;
-  data1.push({ ...req.body, id });
-  fs.writeFile("./db.json", JSON.stringify(data1), (err) => {
+  fs.readFile("./db.json", "utf8", (err, data) => {
     if (err) console.log(err);
-    else res.status(201).json({ message: "Partner added successfully" });
+    else {
+      let id;
+      data = JSON.parse(data);
+      if (data.length === 0) id = 1;
+      else id = data[data.length - 1].id + 1;
+      data.push({ ...req.body, id });
+      fs.writeFile("./db.json", JSON.stringify(data), (err) => {
+        if (err) console.log(err);
+        else res.status(201).json({ message: "Partner added successfully" });
+      });
+    }
   });
 });
 app.delete("/partners/:id", (req, res) => {
   const { id } = req.params;
   let data1;
-  fs.readFile("./db.json", "utf-8", (err, data) => {
+  fs.readFile("./db.json", "utf8", (err, data) => {
     if (err) console.log(err);
-    else data1 = data;
-  });
-  data1 = data1.filter((d) => d.id !== Number(id));
-  fs.writeFile("./db.json", JSON.stringify(data1), (err) => {
-    if (err) console.log(err);
-    else res.status(200).json({ message: "Partner deleted successfully" });
+    else {
+      data = JSON.parse(data);
+      data = data.filter((d) => d.id !== Number(id));
+      fs.writeFile("./db.json", JSON.stringify(data), (err) => {
+        if (err) console.log(err);
+        else res.status(200).json({ message: "Partner deleted successfully" });
+      });
+    }
   });
 });
 app.listen(3000, () => {
